5eef88ed252a51bd0f2648a84256d8c0ba41988b
clairic <k.tsiampa@performance.gr>
2025-10-10 18:56:16 +0300

    Created a resource group and a vnet in azure

---
+++ main.tf
@@ -0,0 +1,35 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+#creating a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-kalliopi-tsiampa"
+  location = "northeurope"
+
+  tags = {
+    Environment = "Terraform Getting Started"
+    location = "northeurope"
+  }
+}
+
+#Create a virtual network inside the resource group
+resource "azurerm_virtual_network" "vnet" {
+  name                = "demoVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "northeurope"
+  resource_group_name = azurerm_resource_group.rg.name
+}
